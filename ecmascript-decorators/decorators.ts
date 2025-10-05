3afc31bb13a73cf81a30da6e77a09f90eabdbd52
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-10-05 12:09:48 +0300

    Completed ECMAScript decorators (logger, autobind, replacer, class/method/field)

--- ecmascript-decorators/decorators.ts
+++ ecmascript-decorators/decorators.ts
@@ -19,13 +19,37 @@ function autobind (
     target: (...args: any[]) => any, 
     ctx: ClassMethodDecoratorContext
 ) {
-    console.log(target);
-    console.log(ctx);
+    ctx.addInitializer(function(this: any) {
+        this[ctx.name] = this[ctx.name].bind(this);
+    });
+
+    return function(this: any) {
+        console.log('Executing original function');
+        target.apply(this);
+    }
+}
+
+function replacer<T>(initValue: T) {
+    return function replacerDecorator(target: undefined, ctx: ClassFieldDecoratorContext) {
+        console.log(target);
+        console.log(ctx);
+
+        return (initialValue: any) => {
+            console.log(initialValue);
+            return initValue;
+        }
+    }
 }
 
 @logger
 class Person {
+
+    @replacer('')
     name = 'Max';
+
+    constructor() {
+        this.greet = this.greet.bind(this);
+    }
 
     @autobind
     greet () {
@@ -38,3 +62,6 @@ const max = new Person();
 // console.log(max);
 
 const julie = new Person();
+
+const greet = max.greet;
+greet();
