7659c85d056e94138b58994d7710e62312553a14
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-10-28 15:26:41 +0200

    feat(zod): add runtime validation and 3rd-party library typings

---
+++ libs/app.ts
@@ -0,0 +1,27 @@
+import _, { values } from 'lodash';
+import { string, z } from 'zod';
+import fs from 'node:fs';
+import { title } from 'node:process';
+
+const numbers = [1, 2, 3, 4, 5];
+
+//split into multiple arrays
+const chunckedArray = _.chunk(numbers, 2);
+
+const dataSchema = z.object({
+    title: z.string(),
+    id: z.number(),
+    values: z.array(z.union([z.string(), z.number()]))
+});
+
+type Data = z.infer<typeof dataSchema>;
+
+function output(data: Data) {
+    console.log(data);
+}
+
+const content = JSON.parse(fs.readFileSync('data.json').toString());
+
+const parsedData = dataSchema.parse(content);
+
+output(parsedData);
