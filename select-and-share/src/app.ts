24e64c2f8102341ee1f2cd018f5a4a4643e8d94b
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-10-31 15:28:17 +0200

    feat(select-and-share): finalize project with Google Maps integration and coordinate fetching

--- select-and-share/src/app.ts
+++ select-and-share/src/app.ts
@@ -4,13 +4,46 @@ const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "";
+const requestUrl = "";
+declare var google: any;
+
+type GoogleGeoodingResponse = {
+    results: {
+        geometry: {
+            location: {
+                lat: number, 
+                lng: number
+            }
+        }
+    }[];
+    status: 'OK' | 'ZERO_RESULTS'
+}
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
 
     const eneteredAddress = addressInput.value;
 
-    axios.get(`url${GOOGLE_API_KEY}`);
+    axios.get<GoogleGeoodingResponse>(
+        `${requestUrl}
+        ${eneteredAddress}
+        ${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+        if(response.data.status !== 'OK'){
+            throw new Error('Could not fetch location');
+        }
+        const coordinates = response.data.results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById('map'), {
+            center: coordinates,
+            zoom: 8
+        });
+        new google.maps.Marker({position: coordinates, map: map});
+    })
+    .catch(err => {
+        alert(err.message);
+        console.log(err);
+    });
 
 }
 
