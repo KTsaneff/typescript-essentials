045cefd91c61023e422d0d2e4f09271b33e030a8
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-11-02 23:07:42 +0200

    Add React + TypeScript project

---
+++ ReactJS+Typescript/react-ts/src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+import Header from "./components/Header.tsx";
+import goalsImg from "./assets/goals.jpg"
+import CourseGoals from "./components/CourseGoals.tsx";
+
+function App() {
+  const [goals, setGoals] = useState(
+    [
+        {
+          id: 1,
+          title: 'Learn TS',
+          description: 'Learn TS form the ground up',
+        },
+        {
+          id: 2,
+          title: 'Build a React + TS App',
+          description: 'Create a small React project using TypeScript and Vite.',
+        },
+        {
+          id: 3,
+          title: 'Master State Management',
+          description: 'Learn how to manage component state effectively with React hooks.',
+        },
+        {
+          id: 4,
+          title: 'Deploy Your Project',
+          description: 'Learn how to deploy your Vite + React app to GitHub Pages or Netlify.',
+        },
+      ]
+  );
+
+  function handleDeleteGoal (id: number) {
+    setGoals((prevGoals) => 
+      prevGoals.filter(g => g.id !== id)
+    );
+  }
+  return(
+    <main>
+    <Header image={{src: goalsImg, alt: 'A list of goals...'}}>
+      <h1>Your Course Goals</h1>
+    </Header>
+    <CourseGoals goals={goals} onDelete={handleDeleteGoal} />
+    </main>
+  );
+}
+
+export default App;
