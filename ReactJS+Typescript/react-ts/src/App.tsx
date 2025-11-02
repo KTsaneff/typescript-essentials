0556638d20d5b9dc35f16896c90e8796b71d9049
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-11-02 23:37:38 +0200

    Finalize React + TypeScript project (completed section exercises)

--- ReactJS+Typescript/react-ts/src/App.tsx
+++ ReactJS+Typescript/react-ts/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg"
 import CourseGoals from "./components/CourseGoals.tsx";
+import NewGoal from "./components/NewGoal.tsx";
 
 function App() {
   const [goals, setGoals] = useState(
@@ -31,15 +32,21 @@ function App() {
   );
 
   function handleDeleteGoal (id: number) {
-    setGoals((prevGoals) => 
-      prevGoals.filter(g => g.id !== id)
-    );
+    setGoals((prevGoals) => prevGoals.filter(g => g.id !== id));
   }
+
+  function handleAddGoal(text: string, summary: string) {
+    setGoals((prevGoals) => 
+      prevGoals.concat({id: Math.random(), title: text, description: summary})
+  );
+  }
+
   return(
     <main>
     <Header image={{src: goalsImg, alt: 'A list of goals...'}}>
       <h1>Your Course Goals</h1>
     </Header>
+    <NewGoal onAdd={handleAddGoal} />
     <CourseGoals goals={goals} onDelete={handleDeleteGoal} />
     </main>
   );
