14c9237a74245cdb24fca350393aa18623498a10
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-11-03 12:35:34 +0200

    Continue Node.js + Express + TypeScript section: added new topics and improvements

--- nodejs-express-ts/app.ts
+++ nodejs-express-ts/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("Hello Udemy!");
+    console.log(req.method);
+    res.json({message: "Good Bye!"});
+});
+
+app.listen(3000);
